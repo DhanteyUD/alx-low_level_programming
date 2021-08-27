63e509336bd704fd88a14af4525a0f0239be4612
DhanteyUD <otseclinton60@gmail.com>
2021-08-27 05:13:52 -0700

    print quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,8 @@
 * main - Entry point
 * Return: Always 0 (Success)
 */
-int main(void) 
-{  
+int main(void)
+{
 printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
